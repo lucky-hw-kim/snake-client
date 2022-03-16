679d73a5db0453f5d8a16c0aabfaf33a1324bde4
Lucky Kim <lucky.hw.kim@gmail.com>
2022-03-16 04:10:03 +0000

    Make connection to a server

---
+++ play.js
@@ -0,0 +1,22 @@
+const net = require('net');
+
+//establishes a connection with the game server
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.2.15',
+    port: 50541
+  });
+
+  conn.on("connect", (data) => {
+    console.log('Server says: ', data);
+  })
+
+  // interpet incoming data as text
+  conn.setEncoding('utf-8');
+
+  return conn;
+};
+
+console.log("Connecting...");
+connect();
